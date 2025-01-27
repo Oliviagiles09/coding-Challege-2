8a6f5daadcba5b39588026a19b1224aace8b787e
Oliviagiles09 <giles11@usf.edu>
2025-01-26 22:51:57 -0500

    task 2 - Students Scores

--- cc_2.js
+++ cc_2.js
@@ -8,3 +8,14 @@ consol.log(products);
 products.pop()
 
 consol.log("Updated products:", products);
+
+// Task 2 
+let scores = [100, 95, 90, 80, 85];
+scores[1] = 85;
+
+let total = scores.reduce((a, b) => a + b, 0);
+let average = total / scores.length;
+
+consol.log("Updated Scores:", scores);
+consol.log(total);
+consol.log("Average Scores:", average);
