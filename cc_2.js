e27d8d65df91223597b6e44b9ab06754be99d62a
Oliviagiles09 <giles11@usf.edu>
2025-01-26 22:44:01 -0500

    Task 1 - Store Inventory

--- cc_2.js
+++ cc_2.js
@@ -1,1 +1,10 @@
+// Task 1
+let products = ["Blush", "Broner", "Mascara", "Eye Shadow", "Powder"];
 
+products.push("Highlighter");
+
+consol.log(products);
+
+products.pop()
+
+consol.log("Updated products:", products);
